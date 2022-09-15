06a0b020087577c5f22451198669f494097030b0
Mrinal9911 <mrinalk123456@gmail.com>
2022-09-15 15:57:52 +0530

    first

---
+++ database/migrations/2022_09_15_081816_create_employee_table.php
@@ -0,0 +1,36 @@
+<?php
+
+use Illuminate\Database\Migrations\Migration;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Support\Facades\Schema;
+
+return new class extends Migration
+{
+    /**
+     * Run the migrations.
+     *
+     * @return void
+     */
+    public function up()
+    {
+        Schema::create('employee', function (Blueprint $table) {
+            $table->id();
+            $table->string('emp_name',20);
+            $table->integer('emp_contact');
+            $table->text('emp_address');
+            $table->string('emp_email',50);
+            $table->date('doj');
+            $table->timestamps();
+        });
+    }
+
+    /**
+     * Reverse the migrations.
+     *
+     * @return void
+     */
+    public function down()
+    {
+        Schema::dropIfExists('employee');
+    }
+};
---
+++ database/migrations/2022_09_15_081945_create_customer_table.php
@@ -0,0 +1,35 @@
+<?php
+
+use Illuminate\Database\Migrations\Migration;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Support\Facades\Schema;
+
+return new class extends Migration
+{
+    /**
+     * Run the migrations.
+     *
+     * @return void
+     */
+    public function up()
+    {
+        Schema::create('customer', function (Blueprint $table) {
+            $table->id();
+            $table->string('cus_name',20);
+            $table->integer('cus_contact');
+            $table->string('cus_address',100);
+            $table->string('cus_email',50);
+            $table->timestamps();
+        });
+    }
+
+    /**
+     * Reverse the migrations.
+     *
+     * @return void
+     */
+    public function down()
+    {
+        Schema::dropIfExists('customer');
+    }
+};
