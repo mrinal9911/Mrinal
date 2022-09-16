868369270bffb58ec93a10466e959314750d645e
Mrinal9911 <mrinalk123456@gmail.com>
2022-09-16 16:23:25 +0530

    multiple data connection

--- app/Http/Controllers/CustomerController.php
+++ app/Http/Controllers/CustomerController.php
@@ -3,8 +3,14 @@
 namespace App\Http\Controllers;
 
 use Illuminate\Http\Request;
+use App\Models\User;
+use Illuminate\Support\Facades\DB;
 
 class CustomerController extends Controller
 {
     //
+    public function first()
+    {
+        return DB::connection('mysql2')->table('IMS.users')->get();
+    }
 }
